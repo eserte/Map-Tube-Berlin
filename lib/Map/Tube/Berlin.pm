f9eaea6c42b165deca321162b929855a8eb8968f
Slaven Rezic <slaven@rezic.de>
2014-12-02 22:43:05 +0100

    Changes, MANIFEST.SKIP, SEE ALSO etc.

--- lib/Map/Tube/Berlin.pm
+++ lib/Map/Tube/Berlin.pm
@@ -1,10 +1,22 @@
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2014 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: srezic@cpan.org
+#
+
 package Map::Tube::Berlin;
 
 $VERSION = '0.01';
 
 =head1 NAME
 
-Map::Tube::Berlin - Interface to the Berlin S- and U-Bahn Map.
+Map::Tube::Berlin - interface to the Berlin S- and U-Bahn map
 
 =cut
 
@@ -20,7 +32,7 @@ with 'Map::Tube';
 =head1 DESCRIPTION
 
 It currently provides functionality to find the shortest route between
-the two given nodes. The map contains both U-Bahn and S-Bahn stations.
+the two given stations. The map contains both U-Bahn and S-Bahn stations.
 
 =head1 CONSTRUCTOR
 
@@ -29,11 +41,11 @@ the two given nodes. The map contains both U-Bahn and S-Bahn stations.
 
 =head1 METHODS
 
-=head2 get_shortest_route()
+=head2 get_shortest_route(I<START>, I<END>)
 
-This method expects two parameters START and END node name. Node name
-is case insensitive. It returns back the node sequence from START to
-END.
+This method expects two parameters I<START> and I<END> station name.
+Station names are case insensitive. The station sequence from I<START>
+to I<END> is returned.
 
     use Map::Tube::Berlin;
     my $tube = Map::Tube::Berlin->new;
@@ -42,9 +54,17 @@ END.
 
     print "Route: $route\n";
 
+=head1 BUGS
+
+It's too slow. Using A* would probably improve the performance.
+
 =head1 AUTHOR
 
 Slaven Rezic
+
+=head1 SEE ALSO
+
+L<Map::Tube>.
 
 =cut
 
