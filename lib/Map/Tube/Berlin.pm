8720cacc4b9b78a26518d08e40dfaa90ba073cd5
Slaven Rezic <slaven@rezic.de>
2015-01-20 22:01:05 +0100

    0.06

--- lib/Map/Tube/Berlin.pm
+++ lib/Map/Tube/Berlin.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2014 Slaven Rezic. All rights reserved.
+# Copyright (C) 2014,2015 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -12,7 +12,7 @@
 
 package Map::Tube::Berlin;
 
-$VERSION = '0.05';
+$VERSION = '0.06';
 
 =head1 NAME
 
